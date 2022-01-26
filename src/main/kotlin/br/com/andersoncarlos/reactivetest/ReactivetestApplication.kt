8be99238f11184f172c3dda791eff81f6e80d6e9
Anderson Carlos <andersoncarlos28@gmail.com>
2022-01-26 01:32:44 -0400

    refactor: removed blank lines

--- src/main/kotlin/br/com/andersoncarlos/reactivetest/ReactivetestApplication.kt
+++ src/main/kotlin/br/com/andersoncarlos/reactivetest/ReactivetestApplication.kt
@@ -8,7 +8,6 @@ import org.springframework.core.io.ClassPathResource
 import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer
 import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator
 
-
 @SpringBootApplication
 open class ReactivetestApplication
 
