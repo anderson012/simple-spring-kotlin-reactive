aa54ab332717596de92faf0a3486d711e81ae025
Anderson Carlos <andersoncarlos28@gmail.com>
2022-01-27 01:17:08 -0400

    chore: add cors

--- backend/src/main/kotlin/br/com/andersoncarlos/reactivetest/config/AppConfig.kt
+++ backend/src/main/kotlin/br/com/andersoncarlos/reactivetest/config/AppConfig.kt
@@ -1,8 +1,18 @@
 package br.com.andersoncarlos.reactivetest.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 open class AppConfig {
-// no config yet
+    @Bean
+    open fun corsConfigurer(): WebMvcConfigurer? {
+        return object: WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/*").allowedMethods("GET", "POST", "PUT", "DELETE")
+            }
+        }
+    }
 }
